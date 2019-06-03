deb37e9f2a58fc529f1504485536f2374c6f107b
Juan Paixao <juanpaixao@live.com>
2019-06-03 18:04:06 -0300

    Implemented all enemies on platform stage

---
+++ Assets/Scripts/Platform/CloudEnemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _rain;
+    private Transform _player;
+    private float _distance;
+    private bool _rainRoutineControl;
+    private Rigidbody2D _rainRB;
+    private Animator _animator;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _player = GameObject.FindObjectOfType<Player>().GetComponent<Transform>();
+        _rain.transform.position = new Vector2(this.transform.position.x - 0.064f, this.transform.position.y - 0.254f);
+        _rain.SetActive(false);
+        _rainRB = _rain.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        _distance = Vector2.Distance((Vector2)_player.position, (Vector2)this.transform.position);
+        Debug.Log(_distance);
+
+        if (_distance < 9)
+        {
+            RainAnimation();
+            if (!_rainRoutineControl)
+            {
+                StartCoroutine(RainRoutine());
+            }
+        }
+        if (_distance >= 9)
+        {
+            _rain.SetActive(false);
+            _rain.transform.position = new Vector2(this.transform.position.x - 0.064f, this.transform.position.y - 0.254f);
+            _rainRoutineControl = false;
+        }
+    }
+    private IEnumerator RainRoutine()
+    {
+        _rainRoutineControl = true;
+        while (_distance < 9)
+        {
+            yield return new WaitForSecondsRealtime(3);
+            RainAnimation();
+        }
+    }
+    private void RainAnimation()
+    {
+        _animator.SetTrigger("Rain");
+    }
+    public void MakeItRain()
+    {
+        _rain.SetActive(true);
+        _rain.transform.position = new Vector2(this.transform.position.x - 0.064f, this.transform.position.y - 0.254f);
+        _rainRB.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Platform/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private string _direction;
+    [SerializeField] private float _speed;
+    private Rigidbody2D _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        EnemyMovement();
+    }
+    private void EnemyMovement()
+    {
+        if (_direction == "left")
+        {
+            _rb.velocity = new Vector2(-_speed * Time.deltaTime, this._rb.velocity.y);
+        }
+        if (_direction == "right")
+        {
+            _rb.velocity = new Vector2(_speed * Time.deltaTime, this._rb.velocity.y);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("ChangeDirectionWall"))
+        {
+            if (_direction == "left")
+            {
+                _direction = "right";
+            }
+            else
+            {
+                _direction = "left";
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (_direction == "left")
+            {
+                _direction = "right";
+            }
+            else
+            {
+                _direction = "left";
+            }
+        }
+    }
+}
